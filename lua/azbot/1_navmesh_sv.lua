56ad508755dad1e0f977ffdd4a2d8e9dcfb2df82
Azurblau <azr_mail@t-online.de>
2017-02-25 23:53:39 +0100

    Compressing upload.

--- lua/azbot/1_navmesh_sv.lua
+++ lua/azbot/1_navmesh_sv.lua
@@ -4,7 +4,14 @@ return function(lib)
 	lib.MapNavMeshPath = lib.MapNavMeshDir .. game.GetMap() .. ".txt"
 	
 	util.AddNetworkString(lib.MapNavMeshNetworkStr)
-	function lib.UploadMapNavMesh(plOrPls) net.Start(lib.MapNavMeshNetworkStr) net.WriteString(lib.MapNavMesh:Serialize()) net.Send(plOrPls) end
+	function lib.UploadMapNavMesh(plOrPls)
+		net.Start(lib.MapNavMeshNetworkStr)
+			local data = lib.MapNavMesh:Serialize()
+			if data ~= "" then data = util.Compress(data) end
+			net.WriteUInt(data:len(), 32)
+			net.WriteData(data, data:len())
+		net.Send(plOrPls)
+	end
 	
 	file.CreateDir(lib.MapNavMeshDir)
 	function lib.SaveMapNavMesh() file.Write(lib.MapNavMeshPath, lib.MapNavMesh:Serialize()) end
