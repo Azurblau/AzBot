6ddeef7bdc526223f2d5c5122ae8163432b8e8ae
David Vogel <Dadido3@aol.com>
2018-04-01 15:49:02 +0200

    Update russian translation

--- lua/zombiesurvival/gamemode/languages/azbot_russian.lua
+++ lua/zombiesurvival/gamemode/languages/azbot_russian.lua
@@ -5,6 +5,7 @@ LANGUAGE.azbot_redeemwave				= "Вы можете написать в чат !hu
 LANGUAGE.azbot_botmapsonly				= "Эта команда включена только на картах с ботами!"
 LANGUAGE.azbot_toolate					= "Слишком поздно для само-воскрешения (вы можете само-воскресится только до волны %u)."
 LANGUAGE.azbot_alreadyhum				= "Вы уже человек!"
+LANGUAGE.azbot_noredeemlasthuman		= "Только один выживший остался, вы не можете воскресить себя больше!"
 
 LANGUAGE.azbot_selfredeemrecenty		= "Вы уже недавно воскресили себя. Попробуйте снова через %u секунд!"
 LANGUAGE.azbot_selfredeemcooldown		= "Вы воскресили себя. Ваше время перезарядки до следующего само-воскрешения %u секунд!"
