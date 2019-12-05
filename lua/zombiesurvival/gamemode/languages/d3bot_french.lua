78bc78e8d9b057a399b932be425472ecacfd7cee
anton crusader <meleemain@mail.ru>
2019-12-05 17:03:33 +0500

    3 maps + fix + French translation

---
+++ lua/zombiesurvival/gamemode/languages/d3bot_french.lua
@@ -0,0 +1,13 @@
+
+translate.AddLanguage("fr", "French")
+
+LANGUAGE.D3bot_redeemwave			= "Tu peux écrire !human avant la vague %u en jouant en tant que survivant."
+LANGUAGE.D3bot_botmapsonly			= "Cette commande est activé seulement sur les cartes pour les bots"
+LANGUAGE.D3bot_toolate				= "C'est trop tard pour les auto-achats (peut se faire avant la vague %u)"
+LANGUAGE.D3bot_alreadyhum			= "Tu es déjà human."
+LANGUAGE.D3bot_noredeemlasthuman	= "Il ne reste plus qu'un survivant, tu ne plus plus faire d'auto-achat"
+LANGUAGE.D3bot_noredeemzombieescape	= "Cette commande est déactivé sur les cartes type zombie escape!"
+
+LANGUAGE.D3bot_selfredeemdisabled	= "La commande auto-achat est désactivé!"
+LANGUAGE.D3bot_selfredeemrecenty	= "Tu as déjà fait un auto-achat. Attends encore %u secondes!"
+LANGUAGE.D3bot_selfredeemcooldown	= "Tu as effectué un auto-achat. Ton delai pour l'auto-achat suivant est de %u secondes!"
