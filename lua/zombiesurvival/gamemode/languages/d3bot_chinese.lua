8c0a5156b02d90f5148ad098a649bb5ad0ec5dc3
anton crusader <meleemain@mail.ru>
2019-12-10 15:53:24 +0500

    added translations
    Korean and Chinese Simplified

---
+++ lua/zombiesurvival/gamemode/languages/d3bot_chinese.lua
@@ -0,0 +1,13 @@
+
+translate.AddLanguage("zh-CN", "Chinese Simplified")
+
+LANGUAGE.D3bot_redeemwave			= "你可以在 %u 回合之前输入 !human 来进行复活."
+LANGUAGE.D3bot_botmapsonly			= "此命令仅可在机器人地图上使用!"
+LANGUAGE.D3bot_toolate				= "此时复活自身已太晚! (只能在第 %u 波之前使用)."
+LANGUAGE.D3bot_alreadyhum			= "你已经是人类!"
+LANGUAGE.D3bot_noredeemlasthuman	= "仅剩下最后一个幸存者!你已无法复活!"
+LANGUAGE.D3bot_noredeemzombieescape	= "该命令在僵尸逃亡下无法使用!"
+
+LANGUAGE.D3bot_selfredeemdisabled	= "复活指令被禁用!"
+LANGUAGE.D3bot_selfredeemrecenty	= "你最近已经复活过一次. 再次复活需要等待 %u 秒!"
+LANGUAGE.D3bot_selfredeemcooldown	= "你已复活. 在下一次复活可用之前你需要冷却 %u 秒!"
