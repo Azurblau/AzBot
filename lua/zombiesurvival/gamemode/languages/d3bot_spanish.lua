6dd0321077620aa4de3de1c205aa895046e37546
Fafy <36455439+Fafy2801@users.noreply.github.com>
2020-01-08 13:35:33 -0300

    Create d3bot_spanish.lua

---
+++ lua/zombiesurvival/gamemode/languages/d3bot_spanish.lua
@@ -0,0 +1,12 @@
+translate.AddLanguage("es", "Spanish")
+
+LANGUAGE.D3bot_redeemwave			= "Puedes usar !human antes de la oleada %u para jugar como sobreviviente"
+LANGUAGE.D3bot_botmapsonly			= "Este comando sólo está habilitado para mapas con bots!"
+LANGUAGE.D3bot_toolate				= "Es muy tarde para redimirte (solo se puede hacer antes de la oleada %u)"
+LANGUAGE.D3bot_alreadyhum			= "Ya eres un humano!"
+LANGUAGE.D3bot_noredeemlasthuman	= "Solo queda un sobreviviente, no puedes redimirte!"
+LANGUAGE.D3bot_noredeemzombieescape	= "Este comando está deshabilitado en mapas de zombie escape!"
+
+LANGUAGE.D3bot_selfredeemdisabled	= "El comando para redimirte está deshabilitado"
+LANGUAGE.D3bot_selfredeemrecenty	= "Podras redimirte otra vez en %u segundos!"
+LANGUAGE.D3bot_selfredeemcooldown	= "Podras redimirte nuevamente en %u segundos!"
