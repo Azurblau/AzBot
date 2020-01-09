f5050690ad77c6514cb03b599d26f727d4ec9099
Blueberry <igory.ygr200@gmail.com>
2020-01-09 14:43:51 +0500

    added Spanish and Turkish translate

---
+++ lua/zombiesurvival/gamemode/languages/d3bot_turkish.lua
@@ -0,0 +1,12 @@
+translate.AddLanguage("tr", "Turkish")
+
+LANGUAGE.D3bot_redeemwave           = "Kurtulan olarak oynamak için %u dalgasından önce insan yazabilirsiniz."
+LANGUAGE.D3bot_botmapsonly          = "Bu komut yalnızca bot haritalarında etkindir!"
+LANGUAGE.D3bot_toolate              = "Kendi kendini kurtarmak için çok geç (sadece %u dalgasından önce yapılabilir)."
+LANGUAGE.D3bot_alreadyhum           = "Zaten insansın!"
+LANGUAGE.D3bot_noredeemlasthuman    = "Sadece bir kurtulan kaldı, artık kendini kullanamazsın!"
+LANGUAGE.D3bot_noredeemzombieescape = "Bu komut zombi kaçış haritalarında devre dışı bırakıldı!"
+
+LANGUAGE.D3bot_selfredeemdisabled   = "Kendini kullanma komutu devre dışı!"
+LANGUAGE.D3bot_selfredeemrecenty    = "Son zamanlarda zaten kendi kendine kullanılmışsın. %u saniye sonra tekrar dene!"
+LANGUAGE.D3bot_selfredeemcooldown   = "Kendi kendini kurtardın. Bir sonraki kendi kendini kullanana kadar geçerli bekleme süren %u saniye!"
