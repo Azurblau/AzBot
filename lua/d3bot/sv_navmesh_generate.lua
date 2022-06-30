5b39e729bbedbe74f1838e028740e319d13d03c3
Bagel™ <djmiller03@twcny.rr.com>
2022-06-30 05:32:09 -0400

    Move link creation to coroutine (yes)
    My game stopped responding many times during testing so I got annoyed and decided to do it in a coroutine. It yields just enough to keep the window responsive to any input.
    An optional callback has been added and it's where I've moved D3bot.UpdateMapNavMeshUiSubscribers

--- lua/d3bot/sv_navmesh_generate.lua
+++ lua/d3bot/sv_navmesh_generate.lua
@@ -6,57 +6,80 @@ local function setPos(node, pos)
 	node:SetParam("Z", round(pos.z))
 end
 
-function D3bot.ConvertNavmesh()
-	PrintMessage(HUD_PRINTTALK, "Starting navmesh conversion...")
+function D3bot.ConvertNavmesh(callback)
+	if D3bot.StartedConversion then return end
+	local co = coroutine.create(function()
+		D3bot.StartedConversion = true
+		PrintMessage(HUD_PRINTTALK, "Starting navmesh conversion...")
 
-	local mapNavMesh = D3bot.NewNavMesh()
+		local mapNavMesh = D3bot.NewNavMesh()
+	
+		local allAreas = navmesh.GetAllNavAreas()
 
-	for _, area in ipairs(navmesh.GetAllNavAreas()) do
-		local node = mapNavMesh:NewNode()
-		setPos(node, area:GetCenter())
+		for k, area in ipairs(allAreas) do
+			local node = mapNavMesh:NewNode()
+			setPos(node, area:GetCenter())
 
-		local northWestCorner = area:GetCorner(0)
-		local southEastCorner = area:GetCorner(2)
+			local northWestCorner = area:GetCorner(0)
+			local southEastCorner = area:GetCorner(2)
 
-		node:SetParam("AreaXMin", northWestCorner.x)
-		node:SetParam("AreaYMin", northWestCorner.y)
-		node:SetParam("AreaXMax", southEastCorner.x)
-		node:SetParam("AreaYMax", southEastCorner.y)
-	end
+			node:SetParam("AreaXMin", northWestCorner.x)
+			node:SetParam("AreaYMin", northWestCorner.y)
+			node:SetParam("AreaXMax", southEastCorner.x)
+			node:SetParam("AreaYMax", southEastCorner.y)
+		end
 
-	PrintMessage(HUD_PRINTTALK, "Nodes placed.")
+		PrintMessage(HUD_PRINTTALK, "Nodes placed.")
 
-	for _, area in ipairs(navmesh.GetAllNavAreas()) do
-		for _, neighbor in ipairs(area:GetAdjacentAreas()) do
-			if math.abs(area:GetCenter().z - neighbor:GetCenter().z) <= 68 then
+		for _, area in ipairs(allAreas) do
+			for k, neighbor in ipairs(area:GetAdjacentAreas()) do
+				if math.abs(area:GetCenter().z - neighbor:GetCenter().z) <= 68 then
 
-				local selectedNode = mapNavMesh:GetNearestNodeOrNil(area:GetCenter())
+					local selectedNode = mapNavMesh:GetNearestNodeOrNil(area:GetCenter())
 
-				if selectedNode then
-					local node = mapNavMesh:GetNearestNodeOrNil(neighbor:GetCenter())
+					if selectedNode then
+						local node = mapNavMesh:GetNearestNodeOrNil(neighbor:GetCenter())
 
-					if node then
-						mapNavMesh:ForceGetLink(selectedNode, node)
+						if node then
+							mapNavMesh:ForceGetLink(selectedNode, node)
+						end
 					end
+				end
+				
+				if (k % 4) == 0 then
+					coroutine.yield()
 				end
 			end
 		end
-	end
 
-	PrintMessage(HUD_PRINTTALK, "Links connected.")
-	PrintMessage(HUD_PRINTTALK, " ")
+		PrintMessage(HUD_PRINTTALK, "Links connected.")
+		PrintMessage(HUD_PRINTTALK, " ")
 
-	D3bot.MapNavMesh = mapNavMesh
+		D3bot.MapNavMesh = mapNavMesh
 
-	PrintMessage(HUD_PRINTTALK, "Complete!")
-	PrintMessage(HUD_PRINTTALK, " ")
-	PrintMessage(HUD_PRINTTALK, "This mesh does not autosave. Save this manually.")
-	PrintMessage(HUD_PRINTTALK, "In order to use the converted navmesh, reload the map.")
-	PrintMessage(HUD_PRINTTALK, "Also, make sure that D3bot.ValveNavOverride in sv_config.lua is set to false.")
+		PrintMessage(HUD_PRINTTALK, "Complete!")
+		PrintMessage(HUD_PRINTTALK, " ")
+		PrintMessage(HUD_PRINTTALK, "This mesh does not autosave. Save this manually.")
+		PrintMessage(HUD_PRINTTALK, "Also, make sure that D3bot.ValveNavOverride in sv_config.lua is set to false.")
+
+		hook.Remove("ConverterCoroutineStep")
+
+		D3bot.StartedConversion = false
+
+		if callback then
+			callback()
+		end
+	end)
+
+	hook.Add("Think", "ConverterCoroutineStep", function()
+		coroutine.resume(co)
+	end)
 end
 
-function D3bot.GenerateAndConvertNavmesh(initPos, onGround)
-	navmesh.Load()
+function D3bot.GenerateAndConvertNavmesh(initPos, onGround, callback)
+	if not navmesh.IsLoaded() then
+		navmesh.Load()
+	end
 	
 	if not navmesh.IsLoaded() then
 		if not onGround then
@@ -76,7 +99,7 @@ function D3bot.GenerateAndConvertNavmesh(initPos, onGround)
 
 	timer.Simple(1, function()
 		if not navmesh.IsGenerating() then
-			D3bot.ConvertNavmesh()
+			D3bot.ConvertNavmesh(callback)
 		end
 	end)
 end
@@ -84,6 +107,7 @@ end
 concommand.Add("d3bot_nav_generate", function(ply, str, args, argStr)
 	if not ply:IsValid() or not ply:IsSuperAdmin() then return end
 
-	D3bot.GenerateAndConvertNavmesh(ply:GetPos(), ply:IsOnGround())
-	D3bot.UpdateMapNavMeshUiSubscribers()
+	D3bot.GenerateAndConvertNavmesh(ply:GetPos(), ply:IsOnGround(), function()
+		D3bot.UpdateMapNavMeshUiSubscribers()
+	end)
 end)
