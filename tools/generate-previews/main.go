f587924b6562d4f5677969d6441361e2f4fac0e5
David Vogel <Dadido3@aol.com>
2023-02-24 13:01:26 +0100

    Change default scale of preview images

--- tools/generate-previews/main.go
+++ tools/generate-previews/main.go
@@ -10,7 +10,7 @@ import (
 var flagSrc = flag.String("src", path.Join(".", "..", "..", "data", "d3bot", "navmesh", "map"), "The source where all navmeshes are read from.")
 var flagDst = flag.String("dst", path.Join(".", "..", "..", "media", "navmesh-previews"), "The destination where all the navmesh preview files are stored.")
 
-var flagScale = flag.Float64("scale", 0.01, "Scale of the image. Unit depends on the output format used. Raster images are based on 1 pixel / mm. Vector images are output in mm directly.")
+var flagScale = flag.Float64("scale", 0.005, "Scale of the image. Unit depends on the output format used. Raster images are based on 1 pixel / mm. Vector images are output in mm directly.")
 
 // TODO: Add flag for output format
 
